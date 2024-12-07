bd0bc0fb90e2d176321b9eb40b77ed0f43ed001c
Rajusavra <savraraju49@gmail.com>
2024-12-07 13:17:38 +0530

    7-12-24  1:20pm

--- Advance Node Js/Projects/PR 8 AdminPanel/controllers/subcategoryController.js
+++ Advance Node Js/Projects/PR 8 AdminPanel/controllers/subcategoryController.js
@@ -6,7 +6,7 @@ const addSubCategory = async (req, res) => {
     let category = await categoryModel.find({ status: "active" });
 
     return res.render("subcategory/addsubcategory", {
-      category: category,
+      category: category
     });
   } catch (error) {
     console.log(error);
@@ -14,43 +14,96 @@ const addSubCategory = async (req, res) => {
   }
 };
 
-const addSubCategoryFieldsPage = async(req, res)=>{
-    try {
-        const {category,subcategory}=req.body
-        await subcategoryModel.create({
-            categoryid:category,
-            subcategory:subcategory
-            
-        })
-        
-        return res.redirect('/subcategory/viewsubcategory')
-        
-    } catch (error) { 
-        console.log(error);
-        return false
-    }
+const addSubCategoryFieldsPage = async (req, res) => {
+  try {
+    const { category, subcategory } = req.body;
+    await subcategoryModel.create({
+      categoryid: category,
+      subcategory: subcategory,
+    });
+
+    return res.redirect("/subcategory/viewsubcategory");
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+};
+
+const viewSubCategory = async (req, res) => {
+  try {
+    const subcategory = await subcategoryModel.find({}).populate("categoryid");
+    return res.render("subcategory/viewsubcategory", {
+      subcategory: subcategory,
+    });
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+};
+
+const deleteSubCategory = async (req, res) => {
+  try {
+    const id = req.query.id;
+    await subcategoryModel.findByIdAndDelete(id);
+    return res.redirect("/subcategory/viewsubcategory");
+  } catch (err) {
+    console.log(err);
+    return false;
+  }
+};
+
+const editSubCategory = async (req, res) => {
+  try {
+      const id = req.query.id;
+      let category = await categoryModel.find({ status: 'active' });
+      const single = await subcategoryModel.findById(id).populate('categoryid');
+      return res.render('subcategory/editsubcategory', {
+          category: category,
+          single: single
+      });
+  } catch (err) {
+      console.log(err);
+      return false;
+  }
 }
 
-const viewSubCategory = async (req, res)=>{
-    try {
-        const subcategory = await subcategoryModel.find({}).populate('categoryid')
-        return res.render('subcategory/viewsubcategory',{
+const updateSubCategory = async (req, res) => {
+  try {
+      const { editid, category, subcategory } = req.body;
+      await subcategoryModel.findByIdAndUpdate(editid, {
+          categoryid: category,
           subcategory: subcategory
-        })
-        
-    } catch (error) {
-        console.log(error);
-        return false
-    }
+      })
+      return res.redirect('/subcategory/viewsubcategory');
+  } catch (err) {
+      console.log(err);
+      return false;
+  }
 }
 
+const changeStatus = async (req, res) => {
+  try {
+    const status = req.query.status;
+    const id = req.query.id;
+    if (status == "active") {
+      await subcategoryModel.findByIdAndUpdate(id, { status: "deactive" });
+      return res.redirect("/subcategory/viewsubcategory");
+    } else {
+      await subcategoryModel.findByIdAndUpdate(id, { status: "active" });
+      return res.redirect("/subcategory/viewsubcategory");
+    }
+  } catch (err) {
+    console.log(err);
+    return false;
+  }
+};
 
 module.exports = {
-    addSubCategory,
-    addSubCategoryFieldsPage,
-    viewSubCategory,
-    // deleteCategory,
-    // editCategory,
-    // updateCategory,
-    // changeStatus
-  };
+  addSubCategory,
+  addSubCategoryFieldsPage,
+  viewSubCategory,
+  deleteSubCategory,
+  editSubCategory,
+  updateSubCategory,
+  changeStatus,
+};
--- Advance Node Js/Projects/PR 8 AdminPanel/routes/subCategoryRoute.js
+++ Advance Node Js/Projects/PR 8 AdminPanel/routes/subCategoryRoute.js
@@ -4,10 +4,17 @@ const passport = require('passport');
 
 const routes=express.Router()
 
-const { addSubCategory, addSubCategoryFieldsPage,viewSubCategory} = require('../controllers/subcategoryController');
+const { addSubCategory,addSubCategoryFieldsPage,viewSubCategory,deleteSubCategory,editSubCategory,updateSubCategory,changeStatus} = require('../controllers/subcategoryController');
 
 routes.use('/addsubcategory',passport.checkUser,addSubCategory);
 routes.post('/addsubcategoryfields',addSubCategoryFieldsPage);
 routes.get('/viewsubcategory',viewSubCategory);
 
+routes.get('/deletesubcategory',deleteSubCategory);
+routes.get('/editsubcategory',editSubCategory);
+routes.post('/updatesubcategory',updateSubCategory);
+
+
+
+routes.get('/changeStatus', changeStatus);
  module.exports=routes
