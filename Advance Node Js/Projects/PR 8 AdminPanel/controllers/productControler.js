4537f46b812ddf227c6ec6b67b0a0739fd3266ca
Rajusavra <savraraju49@gmail.com>
2024-12-11 16:11:02 +0530

    11-12-24     4:10 pm

---
+++ Advance Node Js/Projects/PR 8 AdminPanel/controllers/productControler.js
@@ -0,0 +1,14 @@
+const categoryModel = require("../models/catagoryModel");
+
+const subcategoryModel = require("../models/subcategoryModels");
+
+const exsubcategoryModel = require("../models/exsubcategoryModel");
+
+const productModel = require("../models/productModel");
+
+
+module.exports = {
+
+
+
+};
