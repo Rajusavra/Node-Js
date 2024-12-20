8fbd734ad5fbae05d619de3fe3df99f78cfd4981
Rajusavra <savraraju49@gmail.com>
2024-12-20 12:41:37 +0530

    20-12-24   12:41  Management System

--- Advance Node Js/Practice/Api/config/db.js
+++ Advance Node Js/Practice/Api/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         const connect = await mongoose.connect(
-            `mongodb+srv://savraraju:It4chi@cluster0.lxd5n.mongodb.net/Api`
+            `mongodb+srv://savraraju:It4chi@cluster0.lxd5n.mongodb.net/Blog_Api`
         );
         console.log('Connected to MongoDB');
         
--- Advance Node Js/Practice/Api/routes/userRoute.js
+++ Advance Node Js/Practice/Api/routes/userRoute.js
@@ -3,10 +3,11 @@ const express =  require('express');
 const routes = express.Router();
 
 const {addUser,viewUser,deleteUser,updateUser} = require('../controllers/userController');
+const { verifyToken,Admin } = require('../middleware/Auth');
 
 routes.post('/adduser',addUser);
-routes.get('/viewuser',viewUser);
-routes.delete('/deleteuser/:id',deleteUser);
-routes.put('/updateuser/:id',updateUser);
+routes.get('/viewuser',verifyToken,Admin,viewUser);
+routes.delete('/deleteuser/:id',verifyToken,Admin,deleteUser);
+routes.put('/updateuser/:id',verifyToken,Admin,updateUser);
 
 module.exports = routes;
