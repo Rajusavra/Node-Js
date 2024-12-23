97988d467f65ddd01218c260dbbb1d06bb940a56
Rajusavra <savraraju49@gmail.com>
2024-12-23 10:25:17 +0530

    23-12-24  10:25 ApiProject

---
+++ Advance Node Js/Projects/PR-9 Blog Api/controllers/blogController.js
@@ -0,0 +1,55 @@
+const blogModel = require('../models/blogModel');
+const fs = require('fs');
+
+const addBlog = async (req,res) => {
+    try {
+        const {title,description} = req.body;
+
+        if(!title || !description || !req.file){
+            return res.status(400).send({
+                success : false,
+                message : "all Fields Are Required !!!...",
+            });
+        }
+        
+        const addPost = await blogModel.create({
+            userid : req.user._id,
+            title : title,
+            description:description,
+            image : req.file.path
+        });
+        return res.status(200).send({
+            success : true,
+            message : "Post Added Successfully",
+            addPost
+        });
+
+    } catch (err) {
+        return res.status(400).send({
+            success : false,
+            message: "Error while adding blog",
+        });
+    }
+}
+
+const viewBlog = async (req,res) => {
+    try {
+        const yourPost = await blogModel.find({userid:req.user._id}).populate('userid')
+        return res.status(200).send({
+            success : true,
+            message : "Your post is Fatched Successfully",
+            yourPost
+        })
+    } catch (err) {
+        return res.status(400).send({
+            success : false,
+            message: 'errrrrr'
+        });
+    }
+}
+
+
+module.exports = {
+    addBlog,
+    viewBlog,
+}
---
+++ Advance Node Js/Projects/PR-9 Blog Api/routes/blogRoute.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const routes = express.Router();
+
+const {addBlog,viewBlog} = require('../controllers/blogController')
+
+const { verifyToken,Admin } = require('../middleware/Auth');
+
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads');
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+      cb(null, file.fieldname + '-' + uniqueSuffix)
+    }
+});
+  
+  const upload = multer({ storage: storage }).single('image');
+
+  routes.post('/addblog',verifyToken,upload,Admin,addBlog);
+  routes.get('/viewblog',verifyToken,Admin,viewBlog);
+
+
+module.exports = routes ;
