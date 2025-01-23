01deb28d5354232a691f7aec530a202a16403937
Rajusavra <savraraju49@gmail.com>
2025-01-23 16:28:31 +0530

    23-1-25

---
+++ Advance Node Js/Practice/Instagram-Clone/models/registermodel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const registerSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+})
+
+const Register = mongoose.model('Register', registerSchema);
+module.exports = Register;
